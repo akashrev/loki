d1f9554e9df5bc0efe7a14b011153a57a66dcd73
akashrev <akash.jain@reverieinc.com>
2016-12-01 23:57:39 +0530

    reverie translation engine integrated

--- labs/labs/views.py
+++ labs/labs/views.py
@@ -4,16 +4,19 @@
 from django.shortcuts import render
 import requests, json
 
-
+###################################################################################################################
 # function for labs index webpage
+
 def index(request):
     return render(request, 'index.html', {
             })
 
 
+###################################################################################################################
 # function for the localization function
 
-def translator_request(message):
+
+def translator_engine(message):
     translation_score = []
     translation_list = []
     #message = "Buy puma casual shoes at a discount of 50 at select stores."
@@ -39,14 +42,46 @@ def translator_request(message):
                 for key, value in item.items():  # dictionaries
                     if key == 'translation':
                         translation_list.append(value)  # append translation data in a list
-                    if key == 'translation_score':
-                        output = round(value, 3)
-                        translation_score.append(output)    # append translation_score data in a list
+                    if key == 'engine':
+                        #output = round(value, 3)
+                        translation_score.append(value)    # append translation_score data in a list
     #translation_list = [x.encode('utf-8') for x in translation_list]
-    zipped = zip(translation_list, translation_score)
+    zipped = zip(translation_list, translation_score)   #zipped becomes dictionary ( hindi o/p : engine_name)
     return zipped, json_response
 
 
+def translator_reverie(message):
+    url = "http://beta.auth.revup.reverieinc.com/apiman-gateway/ReverieMobility/Localization/1.0"
+
+    querystring = {"target_lang": "hindi", "source_lang": "english", "domain": "3"}
+
+    headers = {
+        'content-type': "application/json",
+        'rev-api-key': "1gMnmV0vdkBNkS1SY3uaCMkh3cL883MyPNps",
+        'rev-app-id': "com.mobility.android",
+        'cache-control': "no-cache",
+        'postman-token': "c626987f-0ce2-5f6a-7909-321db1bb3fe0"
+    }
+
+    data = {
+        "data": [
+            message
+        ]
+    }
+
+    response = requests.request("POST", url, headers=headers, params=querystring, json=data)
+    json_response = json.dumps(response.json(), ensure_ascii=False, indent=4, sort_keys=True).encode('utf-8')
+    new_json = json.loads(json_response)
+
+    for key, value in new_json.items():
+        for item in value:
+            for key, value in item.items():
+                if key == "outString":
+                    zipped = zip([value], ['reverie'])
+                    return zipped, json_response
+
+
+#######################################
 # function for localization API on labs
 
 def localization(request):
@@ -54,9 +89,13 @@ def localization(request):
     if request.GET.get('submit'):
         message = request.GET.get('search')
 
-        result, json = translator_request(message)
+        engine_result, engine_json = translator_engine(message)
+
+        reverie_result, reverie_json = translator_reverie(message)
+
+        output = engine_result+reverie_result
         return render(request, 'localization.html', {
-            'search': message, 'result': result, 'raw_json': json,
+            'search': message, 'result': output, 'raw_json': engine_json+'\n'+reverie_json,
         })
 
     else:
@@ -66,7 +105,7 @@ def localization(request):
             })
 
 
-###########################################
+####################################################################################################################
 # function for the transliteration function
 
 # function takes input text and language as parameters and sending the request to the Transliteration API

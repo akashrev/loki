b379247307911afc2446c347d6be4aac0de1f963
akashrev <akash.jain@reverieinc.com>
2016-12-20 11:38:03 +0530

    finalized assist output pattern

--- labs/search_assist/views.py
+++ labs/search_assist/views.py
@@ -5,12 +5,11 @@ from django.shortcuts import render
 import requests, json
 
 
+#######################################################################################################################
 # Create your views here.
 def assist_api(message):
 
     url = "http://search.beta.reverieinc.com/search"
-
-    querystring = {"target_lang": "hindi", "source_lang": "english", "domain": "3"}
 
     headers = {
         'Content-Type': 'application/json',
@@ -20,10 +19,11 @@ def assist_api(message):
             "ret_fields": ["entity", "entity_type", "meta"], "pivot_fields": ["meta"], "accuracy": [95]}
 
     response = requests.request("POST", url, headers=headers, json=data)
+    # raw json to render in templates in beautifier form
     json_response = json.dumps(response.json(), ensure_ascii=False, indent=4, sort_keys=True).encode('utf-8')
-    new_json = json.loads(json_response)
     #    return json_response
-    f_set = []                                      # list to return value to the template
+    new_json = json.loads(json_response)
+    f_set = []                                      # list to return values to the template
     for key, entities in new_json.items():          # initial json
         if key == 'entities':                       # accessing data for entities key
 
@@ -35,19 +35,20 @@ def assist_api(message):
                 for item in item_list:                          # iterating items from list
                     for key, value in item.items():             # iterating dictionary from items of list.
                         if key == "category":                   # searching for key 'category'
-                            for key, ivalue in value.items():   # appending 'category' details
+                            for key, ivalue in value.items():  # appending 'category' details
                                 sub_dict[key] = ivalue
-                        elif key == 'terms':                    # appending 'terms' details
+                                sub_dict = {'1': sub_dict}
+                        elif key == 'terms':  # appending 'terms' details
                             for item in value:
                                 for keys, values in item.items():
                                     if keys == 'token':
                                         # print values
-                                        nkey = values
+                                        keyword = values
                                     else:
-                                        sub_dict[nkey] = values
+                                        sub_dict[keyword] = json.dumps(values)
+                                        # sub_dict='{',sub_dict,'}'
+                    f_set.append((sub_dict))  # appending dictinaries to 'f_set' list
 
-                    f_set.append((sub_dict))              # appending dictinaries to 'f_set' list
-#    print f_set
     return f_set, json_response
 
 
